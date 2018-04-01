e7ff4484b2a812987d1c88f27aad4f493a036178
Saumitra Shukla <saumitra90@gmail.com>
2018-04-01 13:17:10 -0700

    Volatile keyword Implementation

--- src/org/dev/multithreading/VolatileDemo.java
+++ src/org/dev/multithreading/VolatileDemo.java
@@ -1,5 +1,37 @@
 package org.dev.multithreading;
 
+import java.util.Scanner;
+
+class Runnable extends Thread {
+
+	private volatile boolean running = true;
+
+	public void run() {
+			while (running) {
+				System.out.println("Thread is running : " + this.getName() + " -- " + this.getPriority());
+			}
+	}
+	
+	public void shutDown() {
+		running = false;
+	}
+
+}
+
 public class VolatileDemo {
+
+	public static void main(String args[]) {
+		
+		Runnable runnable = new Runnable();
+		System.out.println("Press Enter key to stop");
+		runnable.start();
+		
+		Scanner sc = new Scanner(System.in);
+		sc.nextLine();
+		
+		runnable.shutDown();
+		
+
+	}
 
 }
